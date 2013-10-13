5a67322e781e4ccde6e2690828a9a85f20f02f00
Konstantin Makarchev <kostya27@gmail.com>
2013-10-13 22:57:06 +0400

    load all folder in specs?

--- lib/nagios/spec_helper.rb
+++ lib/nagios/spec_helper.rb
@@ -2,6 +2,10 @@ if defined?(RAILS_ENV) && RAILS_ENV['test']
 
   Nagios.load_initializers
 
+  Dir[Nagios.root + "/**/*.rb"].each do |file|
+    require file
+  end
+
   module EM
     def self.defer
       yield
